86d95afe6e33f4cbd2e3508c5c399e318253a585
asungkim <kimasung143@gmail.com>
2025-02-04 15:24:09 +0900

    join b2

--- src/main/java/com/example/rest_tdd/domain/member/member/controller/ApiV1MemberController.java
+++ src/main/java/com/example/rest_tdd/domain/member/member/controller/ApiV1MemberController.java
@@ -1,5 +1,7 @@
 package com.example.rest_tdd.domain.member.member.controller;
 
+import com.example.rest_tdd.domain.member.member.dto.MemberDto;
+import com.example.rest_tdd.domain.member.member.entity.Member;
 import com.example.rest_tdd.domain.member.member.service.MemberService;
 import com.example.rest_tdd.global.dto.RsData;
 import lombok.RequiredArgsConstructor;
@@ -18,9 +20,12 @@ public class ApiV1MemberController {
     record JoinReqBody(String username, String password, String nickname) {}
 
     @PostMapping("/join")
-    public RsData<Void> join(@RequestBody JoinReqBody body) {
-        memberService.join(body.username(), body.password(), body.nickname());
+    public RsData<MemberDto> join(@RequestBody JoinReqBody body) {
+        Member member = memberService.join(body.username(), body.password(), body.nickname());
 
-        return new RsData<>("201-1", "회원가입이 완료되었습니다.");
+        return new RsData<>(
+                "201-1",
+                "회원가입이 완료되었습니다.",
+                new MemberDto(member));
     }
 }
