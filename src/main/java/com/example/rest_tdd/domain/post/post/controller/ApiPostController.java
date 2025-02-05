92924af40b0d8ec538d20f2a09d4192db5d1cbd3
asungkim <kimasung143@gmail.com>
2025-02-05 12:19:30 +0900

    item test g

--- src/main/java/com/example/rest_tdd/domain/post/post/controller/ApiPostController.java
+++ src/main/java/com/example/rest_tdd/domain/post/post/controller/ApiPostController.java
@@ -1,8 +1,27 @@
 package com.example.rest_tdd.domain.post.post.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.example.rest_tdd.domain.post.post.dto.PostDto;
+import com.example.rest_tdd.domain.post.post.entity.Post;
+import com.example.rest_tdd.domain.post.post.service.PostService;
+import com.example.rest_tdd.global.dto.RsData;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/v1/posts")
+@RequiredArgsConstructor
 public class ApiPostController {
 
+    private final PostService postService;
+
+    @GetMapping("/{id}")
+    public RsData<PostDto> getItem(@PathVariable long id) {
+        Post post = postService.getItem(id).get();
+
+        return new RsData<>(
+                "200-1",
+                "%d번 글을 조회하였습니다.".formatted(id),
+                new PostDto(post)
+        );
+    }
 }
