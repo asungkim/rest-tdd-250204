d56eb10b802dab70ae141fbcac312988eebb4b4f
asungkim <kimasung143@gmail.com>
2025-02-05 12:27:11 +0900

    item2 test add - no exist post getItem

--- src/main/java/com/example/rest_tdd/domain/post/post/controller/ApiPostController.java
+++ src/main/java/com/example/rest_tdd/domain/post/post/controller/ApiPostController.java
@@ -4,6 +4,7 @@ import com.example.rest_tdd.domain.post.post.dto.PostDto;
 import com.example.rest_tdd.domain.post.post.entity.Post;
 import com.example.rest_tdd.domain.post.post.service.PostService;
 import com.example.rest_tdd.global.dto.RsData;
+import com.example.rest_tdd.global.exception.ServiceException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,7 +17,9 @@ public class ApiPostController {
 
     @GetMapping("/{id}")
     public RsData<PostDto> getItem(@PathVariable long id) {
-        Post post = postService.getItem(id).get();
+        Post post = postService.getItem(id).orElseThrow(
+                () -> new ServiceException("404-1", "존재하지 않는 글입니다.")
+        );
 
         return new RsData<>(
                 "200-1",
